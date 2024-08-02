10014e2be9a5d1a3cb022764a66649f940eb12a7
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-02 22:23:02 +0530

    operations in js

--- JS_BY_ChaiOrCode/01_Basics/03_converssionOperation.js
+++ JS_BY_ChaiOrCode/01_Basics/03_converssionOperation.js
@@ -36,3 +36,40 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber))
 
+// *********************************Operation *********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);        => 2 ^ 2
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "Hello "
+// let str2 = "Naman"
+
+// str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+
+// console.log(+true);  => bad habit
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2+2
+
+let gamecounter = 100;
+
+// ++gamecounter;  => pre increament
+console.log(++gamecounter)
+// gamecounter++;    => post increament
+console.log(gamecounter++)
