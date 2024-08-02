0c2e7e6874a91c03c4a3488e8bd98dceff647686
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-02 21:47:09 +0530

    02_Datatypes

---
+++ JS_BY_ChaiOrCode/01_Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+console.log(3 + 3);
+console.log("Hitesh");   // code redability should be high
+
+//  Documentation  tc39    &   MDN
+
+//  Data types
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number  => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone Value
+// undefined -> 
+// symbol => unique
+
+
+// object
+
+console.log(typeof(null))
