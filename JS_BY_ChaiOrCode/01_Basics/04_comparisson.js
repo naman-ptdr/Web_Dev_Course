04272a69e6c378de4ac6d5f234345024e61f97b6
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-02 22:32:33 +0530

    04_comparison

---
+++ JS_BY_ChaiOrCode/01_Basics/04_comparisson.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==2);
+// console.log(2!=1);
+// console.log(2==1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log("2" == 2); // => true
+// Here datatype converssion takes place
+
+// strict check
+console.log("2" === 2); //   =>    //false
+// Because their data type is different
