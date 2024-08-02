e4a01b824f68ec2d81bdec6ada1cd75f96278c25
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-02 21:34:59 +0530

    chai or code

---
+++ JS_BY_ChaiOrCode/01_Basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "namuupatidar@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;     // output is undefined
+
+// accountId = 344421      not allowed
+
+accountEmail = "naman@gmail.com"
+accountPassword = "534nfk"
+accountCity = "Mumbai"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+*/
