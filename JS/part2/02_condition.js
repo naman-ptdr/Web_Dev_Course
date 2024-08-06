23e58fc5d4011e9a16a8b179fd54c90e658c2e2b
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-06 21:43:10 +0530

    js part2

---
+++ JS/part2/02_condition.js
@@ -0,0 +1,6 @@
+console.log("Before my if statement : ")
+let age = 19;
+if(age>=18){
+    console.log("You can Vote!");
+}
+console.log("after my if statement ");
---
+++ JS/part2/app.js
@@ -0,0 +1,24 @@
+// console.log("Hello World");
+
+let a = 10;
+let b = 5;
+console.log("sum is : ", a+b);
+
+let pencilPrice = 10;
+let eraserPrice  = 5;
+
+// console.log("The total price is : ", pencilPrice+eraserPrice, "Rupees. ")
+console.log("The total price is :", (pencilPrice+eraserPrice), "Rupees. ")
+
+let output = "The total price is : "+(pencilPrice+eraserPrice) + " Rupees. ";
+console.log(output);
+
+// Template literals
+console.log(`The total price is : ${pencilPrice+eraserPrice} Rupees`);
+
+let age = 18;
+console.log(age>20);
+let c = 5;
+console.log(c=='5');
+
+
