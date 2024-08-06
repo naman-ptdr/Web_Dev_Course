d3e23abf285952e82b8be6f61889c28351f701c0
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-06 21:48:47 +0530

    practice1

---
+++ JS/part2/03_practice.js
@@ -0,0 +1,8 @@
+let color = "green";
+if(color==="red"){
+    console.log(`Stop! Light color is ${color}`);
+}else if(color==="yellow"){
+    console.log(`Slow Down! Light color is ${color}`);
+}else{
+    console.log(`Go! Light color is ${color}`);
+}
