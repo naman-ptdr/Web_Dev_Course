52d187cbb9fef6d1f72276ee655be624f14e5616
Naman Patidar <namanpatidar5555@gmail.com>
2024-08-11 11:49:58 +0530

    Math and num

---
+++ JS_BY_ChaiOrCode/01_Basics/06_numMaths.js
@@ -0,0 +1,50 @@
+const score = 400
+
+const balance = new Number(100)
+
+console.log(score)
+console.log(balance)
+
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(3))
+
+// const otherNumber1 = 123.8966
+// console.log(otherNumber1.toPrecision(3))
+
+// const otherNumber2 = 1123.8966
+// console.log(otherNumber2.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+// -------------------------------MATHS-----------------------------
+
+console.log(Math)
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(4, 6, 2, 9))
+// console.log(Math.max(6, 3, 9, 1))
+
+console.log(Math.random())  // [0, 1]
+// min value will be 1 at below case
+console.log((Math.random()*10) + 1)     // If 0.0__  then + 1
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
+
+//  if we want random number in between a-4 and b-6
+const ran = Math.random()
+console.log(ran)
+console.log(Math.floor(ran * (6-4 + 1)) + 4)
