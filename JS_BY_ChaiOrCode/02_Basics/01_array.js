cfc81c18ae1243c52d45ca6c96632bd021d87582
Naman Patidar <namanpatidar5555@gmail.com>
2024-09-15 00:21:42 +0530

    01_array

---
+++ JS_BY_ChaiOrCode/02_Basics/01_array.js
@@ -0,0 +1,41 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5]    // myArr = [0, 1, 2, 3, 4, "naman", true] is possible in js
+const myHeros = ["BhagatSingh", "ChandraShekharAazad", "Sadguru", "Sukhdev", "ShivajiRaje"]
+const myArr2 = new Array(1, 2, 3, 4, 5)
+console.log(myArr[2]);
+
+
+// Array Methods
+
+myArr.push(6)
+myArr.push(8)
+
+myArr.pop()
+
+myArr.unshift(9)  // it shift all values of array and insert 9 at begining
+myArr.shift() // remove first element and shift
+
+console.log(myArr.includes(11));
+console.log(myArr.indexOf(3));
+
+console.log(myArr);
+const newArr = myArr.join()
+console.log(newArr);
+console.log(typeof(myArr));
+console.log(typeof(newArr));
+
+
+//  slice , splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1)
+console.log("B", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2);
+console.log("C", myArr);
+
