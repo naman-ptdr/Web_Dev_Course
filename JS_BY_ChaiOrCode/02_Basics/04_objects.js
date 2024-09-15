adc77654eb9ccec22c20c5219f680af40908a312
Naman Patidar <namanpatidar5555@gmail.com>
2024-09-15 17:39:27 +0530

    04_objects

---
+++ JS_BY_ChaiOrCode/02_Basics/04_objects.js
@@ -0,0 +1,67 @@
+const tinderUser = new Object()         //  singleton object
+// const tinderUser = {}     both are same         literals
+
+
+tinderUser.id = "123abc"
+tinderUser.name = "Naman"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "naman@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "Naman",
+            lastname : "Patidar"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.lastname);
+
+
+// combine two objects 
+
+
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {3 : "c", 4 : "d"}
+
+// const obj3 = {obj1, obj2}  // in this obj3 contains obj 1 and 2 not mearge them
+
+// const obj3 = Object.assign({}, obj1, obj2)     // {} - target  and in this empty object obj1 and 2 are added  
+const obj3 = {...obj1, ...obj2}  //  same as above one
+
+
+// const obj3 = Object.assign(obj1, obj2)    //in this in obj 1 obj2 is added
+console.log(obj1);
+
+
+console.log(obj3);
+
+
+const users = [
+    {
+        id : 1,
+        email : "n@gmail.com"
+    },
+    {
+        id : 2,
+        email : "p@gmail.com"
+    },
+    {
+        id : 3,
+        email : "a@gmail.com"
+    }
+]
+
+console.log(users[1].email);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
