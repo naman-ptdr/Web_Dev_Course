c322b472bd235b24d83bb6f7787cad32dd0a4b77
Naman Patidar <namanpatidar5555@gmail.com>
2024-09-16 18:27:21 +0530

    02_functions

---
+++ JS_BY_ChaiOrCode/03_Basics/02_functions.js
@@ -0,0 +1,41 @@
+// if we didn't know about how many arrguments are there 
+
+// rest or spread operator in js  -->  ...
+function calcCartPrice(...num){
+    return num
+}
+
+console.log(calcCartPrice(200, 400, 500));
+console.log(calcCartPrice(200, 400, 500, 600, 700));
+console.log(calcCartPrice(200, 400, 900));
+
+function calcCartPrice(val1, val2, ...num1){
+    return num1
+}
+// val1 = 100 and val2 = 200
+console.log(calcCartPrice(100, 200, 300, 400, 500, 600, 700));
+
+const user = {
+    username : "Naman",
+    product : "Iwatch",
+    price : 40000
+}
+
+function handleObjects(anyObject){
+    console.log(`User Name is ${anyObject.username} and item name ${anyObject.product} price is ${anyObject.price}`);
+}
+
+handleObjects(user)
+handleObjects({
+    username : "Rupesh", 
+    product : "Iphone",
+    price : 150000
+})
+
+const myNewArray = [200, 400, 600, 800]
+function returnThirdValue(getArray){   // getArray is just parameter name we can write anything
+    return getArray[2]
+}
+
+console.log(returnThirdValue(myNewArray));
+
