63169c5c10aab39f11badc93f63457bbf087c473
Naman Patidar <namanpatidar5555@gmail.com>
2024-09-16 19:37:05 +0530

    Scopes and mini Hosting

--- JS_BY_ChaiOrCode/03_Basics/03_scopes.js
+++ JS_BY_ChaiOrCode/03_Basics/03_scopes.js
@@ -20,3 +20,54 @@ console.log(`Outer a : ${a}`);
 console.log(`Outer b : ${b}`);
 console.log(`Outer c : ${c}`);
 
+
+// Nested scopes
+
+// Closure ----  child function can access perent function variables
+function one(){
+    const username = "Naman"
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if(true){
+    const username = "Naman"
+    if(username==="Naman"){
+        const website = " Youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+//---------------------Interesting Question ------------------------
+//HOSTING IN JS
+
+
+// We can call function before its deffination 
+console.log(addOne(5));
+
+function addOne(num){   // this is called as function 
+    return num+1
+}
+
+// addOne(5)
+
+//  But can not call expression before function deffination . 
+// This is called ***Hosting*** that how to declare functions , how they store , what their execussion context 
+
+// console.log(addTwo(5));
+
+const addTwo = function(num){     // this is also a function but it can be called as expression.  This variables can hold functions , JSON values
+    return num+2
+}
+
+console.log(addTwo(5));
