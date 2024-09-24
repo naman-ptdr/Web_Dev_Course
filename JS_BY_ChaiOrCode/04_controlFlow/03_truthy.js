c1f676456a6b08f91d2ca57255dbee148af6705b
Naman Patidar <namanpatidar5555@gmail.com>
2024-09-24 21:44:36 +0530

    03_truthy

---
+++ JS_BY_ChaiOrCode/04_controlFlow/03_truthy.js
@@ -0,0 +1,40 @@
+const userEmail = "n@naman.ai"
+
+if(userEmail){
+    console.log("Got user email");
+}else{
+    console.log("Dont have user email");  
+}
+
+
+//  falsy value
+
+// false, 0, -0, BigInt 0n, "", null , undefined, NaN
+
+// Truthy value
+
+// [], "0", 'flse', ' ', " ", {}, function(){}
+
+// const arr = []
+// if(arr.length === 0) console.log("Array is empty");
+
+
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length === 0) console.log("Object is empty");
+
+
+// Nullish Coalescing Operator(??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+console.log(val1);
+
+
+// Terniary operator
+//  condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Less then 80"): console.log("more then 80");
+;
