3174fc5673ebcaf3795c101cb70e6cb83ad3fad5
Naman Patidar <namanpatidar5555@gmail.com>
2024-09-25 22:52:39 +0530

    Reduce method

--- JS_BY_ChaiOrCode/05_Iteration/08_Reduce.js
+++ JS_BY_ChaiOrCode/05_Iteration/08_Reduce.js
@@ -1,9 +1,40 @@
 // Reduce method in js
 
 const myNums = [1, 2, 3]
-// acc - accumlator , currval - currentValue
-const myTotal = myNums.reduce(function(acc, currval){
-    return acc + currval
-}, 0)     // acc start from 0
 
+// acc - accumlator , currval - currentValue
+
+// const myTotal = myNums.reduce(function(acc, currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 3)     // acc start from 0
+
+// console.log(myTotal);
+
+
+const myTotal = myNums.reduce((acc, currval) => acc+currval, 0)
 console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js Course",
+        price: 2999
+    },
+    {
+        itemName: "python Course",
+        price: 5999
+    },
+    {
+        itemName: "Mobile dev",
+        price: 9999
+    },
+    {
+        itemName: "Data science Course",
+        price: 12999
+    }
+];
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
