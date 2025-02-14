862279b138628941ccfcbe863e37b3263cb53d78
Naman Patidar <namanpatidar5555@gmail.com>
2025-02-14 23:07:41 +0530

    string

--- JS_BY_ChaiOrCode/01_Basics/05_string.js
+++ JS_BY_ChaiOrCode/01_Basics/05_string.js
@@ -3,25 +3,28 @@ const repoCount = 50
 
 // console.log(name + repoCount + "Value");
 
+// using backticks
 console.log(`Hello my name is ${name} ans my repo count is ${repoCount} ${"Value"}`)
 
 //  object of string
 const gameName = new String('Naman-Patidar')  //contructor
 
-console.log(typeof(name))
-console.log(typeof(gameName))
+console.log(typeof(name))       // string
+console.log(typeof(gameName))   // object 
 
 console.log(gameName[0])
-console.log(gameName.__proto__)
+console.log(gameName.__proto__)     // in browser check prototype by inspect
 
 console.log(gameName.length)
-console.log(gameName.toUpperCase())
+console.log(gameName.toUpperCase())     // not chenged original value
+console.log(String(gameName));
+
 console.log(gameName.charAt(2))
 console.log(gameName.indexOf('a'))
 
-const newString = gameName.substring(0,4)
+const newString = gameName.substring(0,4)    // excluding 4 th index
 console.log(newString)
-const anotherString = gameName.slice(-8,6)
+const anotherString = gameName.slice(-8,7) // end sai 8th index and front sai 7th
 console.log(anotherString)
 
 
@@ -30,12 +33,13 @@ console.log(newStringOne)
 console.log(newStringOne.trim())
 
 
-const url = "https://naman.com/patidar%20tejra"
+const url = "https://naman.com/patidar%20tejra"  // if someone enter -"https://naman.com/patidar tejra"   ---- 
+// browser space ko %20 mai replace kar deta hai and we want to change space by -
 console.log(url.replace('%20', '-'))
 
 console.log(url.includes('hay'))
 
-const str = "The quick brown fox jumps over the lazy dog."
+const str = new String("The quick brown fox jumps over the lazy dog.")
 // console.log(str.split(' '));
 const words = str.split(' ');
 console.log(words[3])
