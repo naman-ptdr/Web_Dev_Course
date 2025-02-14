913e6c5cbac7fbb0c24b27b34b8644027c3c1bf1
Naman Patidar <namanpatidar5555@gmail.com>
2025-02-15 00:31:38 +0530

    array

--- JS_BY_ChaiOrCode/02_Basics/02_array.js
+++ JS_BY_ChaiOrCode/02_Basics/02_array.js
@@ -6,12 +6,18 @@ const dc_char = ["Superman", "Flash", "Batman", "Deadpool"]
 
 // const all_char = marvel_char.concat(dc_char)
 
+// also by spread operator
 const all_char = [...marvel_char, ...dc_char]
 console.log(all_char);
 
 const another_arr = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
-const useable_another_arr = another_arr.flat(Infinity)
-console.log(useable_another_arr);
+const useable_another_arr1 = another_arr.flat(Infinity)  // infinity is array depth
+const useable_another_arr2 = another_arr.flat(1)
+const useable_another_arr3 = another_arr.flat(2)
+console.log(useable_another_arr1);
+console.log(useable_another_arr2);
+console.log(useable_another_arr3);
+
 
 
 console.log(Array.isArray("Naman"));
