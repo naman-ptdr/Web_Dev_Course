b2202fae5537a2413bddecd9908d6d3046be2605
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-06 08:30:34 +0530

    promises

--- JS_BY_ChaiOrCode/09_advance_one/02_promises.js
+++ JS_BY_ChaiOrCode/09_advance_one/02_promises.js
@@ -43,8 +43,29 @@ const promiseFour = new Promise(function(resolve, reject){
     }, 1000)
 })
 
-promiseFour.then((user)=>{
+promiseFour
+.then((user)=>{
     console.log(user);
     return user.username;
-    
+}).then((username)=>{
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(()=> console.log("The promise is either resolved or reject"))
+
+
+const promisFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "javascript", password: "1234"})
+        }else{
+            reject('ERROR : Somthing went wrong')
+        }
+    },1000)
 })
+
+async function consumePromisefive() {
+    const response = await promisefive;
+    console.log(response);
+}
