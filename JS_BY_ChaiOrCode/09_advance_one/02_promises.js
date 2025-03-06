b091831c69387200830b50f7936c826cb98b9d47
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-06 08:20:03 +0530

    API request

---
+++ JS_BY_ChaiOrCode/09_advance_one/02_promises.js
@@ -0,0 +1,50 @@
+const promisOne = new Promise(function(resolve, reject){
+    //Do an async task
+    //DB calls, cryptography , network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    }, 1000)
+})
+
+promisOne.then(function(){
+    console.log("Promise consumed");
+})
+
+
+new promisOne(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Naman", email: "namuu@gmail.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "naman", password: "1234"})
+        }else{
+            reject('ERROR : Somthing went wrong')
+        }
+        
+    }, 1000)
+})
+
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username;
+    
+})
