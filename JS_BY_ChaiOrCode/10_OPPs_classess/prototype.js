220cfb55413e01537debaeda01de34feab23ead7
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-08 17:31:32 +0530

    Prototypes

---
+++ JS_BY_ChaiOrCode/10_OPPs_classess/prototype.js
@@ -0,0 +1,16 @@
+// Js is a prototype based language that means js aasani 
+// sai haar nahi manti usse jabtak uski value nahi milti 
+// tab tak voh uske parent layer par dekhegi then grant 
+// parent then uske bhi uper jab tak ki usse null value 
+// na mile
+
+// this is called prototype behaiviar of JS and prototypal inheritance
+
+// js mai har ek chij object hai like array , string etc or jo properties 
+// array kai paas hai vo lagbhag string kai paas bhi hogi 
+// kyoki inka grand parent is an object.
+
+      //   array --->   object  ----> null
+     //                  ^
+    //                   |
+   //                 string
