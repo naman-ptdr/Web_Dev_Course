5ad86406e70434a35f4ebbaaf6983280349e11a8
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-08 12:13:50 +0530

    fetch

--- JS_BY_ChaiOrCode/09_advance_one/02_promises.js
+++ JS_BY_ChaiOrCode/09_advance_one/02_promises.js
@@ -92,3 +92,26 @@ async function consumePromiseFive(){
 }
 
 consumePromiseFive();
+
+
+// async function getAllUsers(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+// getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+}).catch((error)=>{
+    console.log(error);
+})
