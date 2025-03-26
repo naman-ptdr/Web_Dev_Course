2e263d53f188d990e6d359d37b73befab80623db
Naman Patidar <namanpatidar5555@gmail.com>
2025-03-26 16:42:59 +0530

    Simon Game started

---
+++ Delta/JS/29_js(mini Project)/simon Game/app.js
@@ -0,0 +1,11 @@
+let gameSeq=[];
+let userSeq=[];
+
+let started = false;
+let level = 0;
+
+document.addEventListener("keypress", function(){
+    if(started==false){
+        console.log("Game Started");
+    }
+})
