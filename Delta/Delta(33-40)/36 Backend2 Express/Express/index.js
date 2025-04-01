fb09285635ae88f0d236756489aa111ef5e4b35b
Naman_ptdr <namanpatidar5555@gmail.com>
2025-04-01 19:39:47 +0530

    Backend 2 Express

---
+++ Delta/Delta(33-40)/36 Backend2 Express/Express/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+// console.dir(app);
+
+
+let port = 8080;
+app.listen(port, ()=>{
+    console.log(`app is listening on port ${port}`);
+});
+
+// app.use((req, res)=>{
+//     // console.log(req);
+//     // res.send("This is a basic Response");
+//     console.log("Request received");
+//     let code = "<h1>Fruits</h1> <ul><li>apple</li><li>orange</li></ul>"
+//     res.send(code);
+// });
+
+app.get("/", (req, res) => {
+    res.send("Hello, i am root");
+});
+
+app.get("/:username/:id", (req, res) => {
+    let {username, id} = req.params;
+    res.send(`Wlcome to the page of @${username}`);
+});
